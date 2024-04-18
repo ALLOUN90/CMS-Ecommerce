e6f9fde3c77530e085ba4094e3c256d3f0b766db
alloun mohamed <mohamed.moubah123@gmail.com>
2024-04-18 14:47:50 +0100

    18/04

---
+++ CMS-Ecommerce/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,20 @@
+﻿using CMS_Ecommerce.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CMS_Ecommerce.Area.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PagesController(DataContext context) : Controller
+    {
+        private readonly DataContext _context = context;
+        public async Task<IActionResult> Index()
+        {
+            
+           List<Page> pages = await _context.Pages.ToListAsync();
+            
+            return View(pages);
+        }
+    }
+}
